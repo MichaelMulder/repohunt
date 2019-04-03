537a87b13bac8a560d776382f21affc636c9ba8a
Michael Mulder <michaelorenmulder@gmail.com>
2019-04-03 12:10:58 -0500

    base layout and tested api

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,58 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-toolbar app dark>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Repo Hunt</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-flex xs12 sm6>
+        <v-text-field label="Search GitHub Repos" append-icon="search" v-model="searchTerm"></v-text-field>
+      </v-flex>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <ul v-if="repos && repos.length">
+        <li v-for="repo of repos" :key="repo.id">
+          <p>
+            <strong>{{repo.name}}</strong>
+          </p>
+          <p>{{repo.decscription}}</p>
+        </li>
+      </ul>
+
+      <ul v-if="errors && errors.length">
+        <li v-for="error of errors">{{error.message}}</li>
+      </ul>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "App",
+
+  data() {
     return {
-      //
-    }
+      repos: [],
+      errors: [],
+      searchTerm: ""
+    };
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios
+      .get(`https://api.github.com/search/repositories?q=react`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.repos = response.data.items;
+        console.log(response.data.items);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   }
-}
+};
 </script>
---
+++ src/components/RepoCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <v-flex v-for="repo in repos" :key="repo.title" v-bind="{ [`xs${repo.flex}`]: true }">
+          <v-card>
+            <v-img :src="repo.src" height="200px">
+              <v-container fill-height fluid pa-2>
+                <v-layout fill-height>
+                  <v-flex xs12 align-end flexbox>
+                    <span class="headline white--text" v-text="repo.title"></span>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>favorite</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>note_add</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>share</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
